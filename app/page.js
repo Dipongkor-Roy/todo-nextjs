dcb1cad977f5059da6d20136a5549d8f2f8a5b49
Dipongkor-Roy <juniordipu@gmail.com>
2023-11-05 00:10:03 +0600

    finally basic todo done and explore next also done

--- app/page.js
+++ app/page.js
@@ -1,27 +1,37 @@
-"use client"; // To inform next js, this is a client component 
+"use client"; // To inform next js, this is a client component
 import { useState } from "react";
 
 const page = () => {
-  const[title,setTitle]=useState("")
-  const [desc,setDesc]=useState("")
-  const [mainTask,setMainTask]=useState([])
-  const submitHandle=(event)=>{
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
+  const submitHandle = (event) => {
     event.preventDefault();
-    setMainTask([...mainTask,{title,desc}])//object in array.stored data like an object  
-    setTitle('');
-    setDesc('');
+    setMainTask([...mainTask, { title, desc }]); //object in array.stored data like an object
+    setTitle("");
+    setDesc("");
+  };
+  const deleteHandler=(i)=>{
+    const copyArr=[...mainTask];
+    copyArr.splice(i,1);//i=whice one and 1 for deleteCount
+    setMainTask(copyArr);
   }
-  let renderTask='No Task Available';
-  if(mainTask>0){ // condition for no task
-    renderTask= mainTask.map((t,i)=>{
-      return <li>
-        <div className="flex justify-between">
-        <h2 className="text-2xl font-semibold">{t.title}</h2>
-        <h2 className="text-xl font-semibold">{t.desc}</h2>
-  
-      </div>
-      </li>
-    })
+
+  let renderTask = "No Task Available";
+  if (mainTask.length > 0) {
+    // condition for no task
+    renderTask = mainTask.map((t, i) => {
+      return (
+        //key give unique identity to every task
+        <li key={i} className="flex items-center justify-between mb-5"> 
+          <div className="flex justify-between mb-2 w-2/3">
+            <h2 className="text-2xl font-semibold">{t.title}</h2>
+            <h2 className="text-xl font-semibold">{t.desc}</h2>
+          </div>
+          <button onClick={()=>deleteHandler(i)} className="bg-orange-200 rounded-xl px-3 py-2 ">Delete</button>
+        </li>
+      );
+    });
   }
   return (
     <>
@@ -33,10 +43,10 @@ const page = () => {
           id=""
           className="text-2xl border-zinc-800 border-2 m-8 p-x-4 py-2 "
           placeholder=" Input Task Name"
-        value={title}
-        onChange={(e)=>{
-          setTitle(e.target.value)
-        }}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
         <input
           type="text"
@@ -45,8 +55,8 @@ const page = () => {
           className="text-2xl border-zinc-800 border-2 m-8 p-x-4 py-2 "
           placeholder=" Description"
           value={desc}
-          onChange={(e)=>{
-            setDesc(e.target.value)
+          onChange={(e) => {
+            setDesc(e.target.value);
           }}
         />
         <button className="bg-black px-3 py-2 text-white rounded-md font-mono items-center">
@@ -62,4 +72,3 @@ const page = () => {
 };
 
 export default page;
- 
